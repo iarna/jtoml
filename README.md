1764adfe68cb7543acea93d678f5531cde0eb44a
Rebecca Turner <me@re-becca.org>
2018-06-22 18:19:56 -0700

    initial code

---
+++ README.md
@@ -0,0 +1,43 @@
+# jtoml
+
+Use the [`json`](https://www.npmjs.com/package/json) cli to read and query your TOML
+
+## DESCRIPTION 
+
+It works just like [`json`](https://www.npmjs.com/package/json), but with
+TOML.  Tries to output as TOML, but falls back to JSON for things that can't
+be represented as TOML (bare arrays, values).
+
+In fact, it _literally_ rus `json` under the hood.  It converts TOML to
+JSON, feeds that to `json`, then converts the output of `json` back to TOML
+and displays it.
+
+## EXAMPLES
+
+```
+$ echo 'foo="bar"}' | json
+foo = "bar"
+
+$ echo 'foo="bar"' | json foo
+bar
+
+$ echo 'fred = {age=42}' | json fred.age    # '.' for property access
+42
+
+$ echo '[fred]
+age = 42' | json fred.age    # '.' for property access
+42
+
+$ echo 'age = 10' | json -e 'this.age++'
+{
+  "age": 11
+}
+```
+
+## DIFFERECNES AND UNSUPPORTED FEATURES
+
+`-f`, `--file` - Because this is a thing wrapper around `json`, if you
+specify a file then `json` will load it as, well, JSON, and not TOML.
+
+Streaming mode makes no sense for TOML, there's no way to do an equivalent
+of NDJSON, and there's no document separator to spilt on.
---
+++ jtoml.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+'use strict'
+require('@iarna/cli')(main)
+const TOML = require('@iarna/toml')
+const fs = require('fs')
+const fun = require('funstream')
+const util = require('util')
+
+async function main (opts, ...args) {
+    const realStdin = fun(process.stdin)
+    delete process.stdin
+    process.stdin = realStdin.grab(_ => JSON.stringify(TOML.parse(_.join(''))))
+    process.stdin.isTTY = realStdin.isTTY
+
+    const realStderr = process.stderr
+    let seenOutput = false
+    delete process.stderr
+    process.stderr = fun(out => out.map(_ => {
+      if (!seenOutput) {
+        seenOutput = true
+        realStdin.end()
+      }
+      return _
+    }).pipe(realStderr))
+    console.warn = function (...args) {
+      process.stderr.write(args.map(_ => typeof _ === 'string' ? _ : util.inspect(_)).join(' ') + '\n')
+    }
+    const realStdout = process.stdout
+    delete process.stdout
+    process.stdout = fun(out => out.map(_ => {
+      if (!seenOutput) {
+        seenOutput = true
+        realStdin.end()
+      }
+      return _
+    }).map(_ => {
+      try {
+        return TOML.stringify(JSON.parse(_))
+      } catch (ex) {
+        return _
+      }
+    }).pipe(realStdout))
+    console.log = function (...args) {
+      process.stdout.write(args.map(_ => typeof _ === 'string' ? _ : util.inspect(_)).join(' ') + '\n')
+    }
+//    process.stdout = fun(out => out.grab(_ => TOML.stringify(JSON.parse(_.join('')))).pipe(realStdout))
+  return require('json').main(process.argv)
+}
